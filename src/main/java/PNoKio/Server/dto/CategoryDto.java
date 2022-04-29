eb000399c6328a6820c9697c2a3e4f97ec00b706
rlaehdals <rkdlem48@gmail.com>
2022-04-29 17:24:08 +0900

    fix springboot version

--- src/main/java/PNoKio/Server/dto/CategoryDto.java
+++ src/main/java/PNoKio/Server/dto/CategoryDto.java
@@ -3,8 +3,12 @@ package PNoKio.Server.dto;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotEmpty;
+
 @Data
 @AllArgsConstructor
 public class CategoryDto {
+    @NotEmpty(message= "공란으로 제출할 수 없습니다.")
     private String categoryName;
 }
